f17ff53670455fd0621065aa5c4930090d2a26ed
uncpleo <leonardocqtc@gmail.com>
2024-02-23 00:25:20 -0500

    arreglado del diseño

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let contador = 1;
 
 btnAgregar.addEventListener('click', e=> {
     let cadaDiv = document.createElement('div');
-    cadaDiv.innerHTML = `<div class="d-flex flex-md-col flex-wrap flex-sm-col mb-2"><label class="col-md-3 col-12 form-label">Interesado N° ${contador++}</label> - <input class="col-md-6 col-12 form-control" name="nombre[]" placeholder="Escriba el Nombre"></input><button class="col-md-3 col-12 btn btn-secondary btn-sm mb-2" onclick="eliminar(this)">Eliminar</button></div>`;
+    cadaDiv.innerHTML = `<div class="row"><div class="col-md-3 col-12 p-1"><label class="form-label">Interesado N° ${contador++}</label></div><div class="col-md-6 col-12 p-1"><input class="form-control" name="nombre[]" placeholder="Escriba el Nombre"></input></div><div class="col-md-3 col-12 p-1 d-md-flex justify-content-md-end"><button class="btn btn-secondary btn-sm mb-2" onclick="eliminar(this)">Eliminar</button></div></div>`;
     contenedor.appendChild(cadaDiv);
 })
 
@@ -15,15 +15,18 @@ btnAgregar.addEventListener('click', e=> {
 
 const eliminar = (e) => {
     const divPadre = e.parentNode;
-    contenedor.removeChild(divPadre);
+    const divPadredelPadre = divPadre.parentNode;
+    const divPadredelPadredelPadre = divPadredelPadre.parentNode;
+    contenedor.removeChild(divPadredelPadredelPadre);
     actualizarContenedor();
 }
 
 const actualizarContenedor = () => {
     let divs = contenedor.children;
+    let divs_menor = divs.children;
     contador = 1;
-    for (let i=0; i<divs.length; i++){
-        divs[i].children[0].innerHTML = `Interesado N° ${contador++}`;
+    for (let i=0; i<divs_menor.length; i++){
+        divs_menor[i].children[0].innerHTML = `Interesado N° ${contador++}`;
     }
 }
 
